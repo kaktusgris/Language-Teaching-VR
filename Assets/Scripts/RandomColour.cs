dee785d36302b641c0a835e65008d2494aba5dcf
Plohne <plohne95@gmail.com>
2018-10-24 15:40:01 +0200

    Random colour on head when login, and a body under the head

---
+++ Assets/Scripts/RandomColour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomColour : MonoBehaviour {
+
+    [SerializeField] private bool selectColourManually;
+    [SerializeField] private Color colour;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Renderer>().material.color = generateColour();
+    }
+
+    private Color generateColour()
+    {
+        if (selectColourManually)
+        {
+            return colour;
+        } else
+        {
+            return getRandomColour();
+        }
+    }
+
+    // Returns a random colour
+    public Color getRandomColour()
+    {
+        float r = Random.value;
+        float g = Random.value;
+        float b = Random.value;
+        print(r + ", " + g + ", " + b);
+        return new Color(r, g, b);
+    }
+}
