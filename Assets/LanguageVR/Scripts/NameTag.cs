08c7545d0937af87eab6d686433afa0220a82acd
Plohne <plohne95@gmail.com>
2019-01-22 14:18:08 +0100

    Added name tag over head facing other players

---
+++ Assets/LanguageVR/Scripts/NameTag.cs
@@ -0,0 +1,37 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NameTag : MonoBehaviour, IPunObservable
+{
+    private string name;
+
+    private TextMesh textMesh;
+
+    private void Awake()
+    {
+        textMesh = GetComponent<TextMesh>();
+        name = PhotonNetwork.NickName;
+        textMesh.text = name;
+    }
+
+    private void Update()
+    {
+        Transform eyes = ViveManager.Instance.head.transform;
+        textMesh.transform.LookAt(eyes);
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(name);
+        }
+        else
+        {
+            name = (string) stream.ReceiveNext();
+            textMesh.text = name;
+        }
+    }
+}
