6e7fbfeac5ede9aa6a599a80c12c4164f0aa2d6e
Plohne <plohne95@gmail.com>
2019-02-25 11:36:23 +0100

    Prefabs and scripts in lobby will now not be connected to PUN Demo

--- Assets/LanguageVR/Scripts/Lobby/PlayerEntry.cs
+++ Assets/LanguageVR/Scripts/Lobby/PlayerEntry.cs
@@ -41,7 +41,7 @@ namespace NTNU.CarloMarton.VRLanguage
                     isPlayerReady = !isPlayerReady;
                     SetPlayerReady(isPlayerReady);
 
-                    Hashtable props = new Hashtable() {{"IsPlayerReady", isPlayerReady}};
+                    Hashtable props = new Hashtable() { { "IsPlayerReady", isPlayerReady } };
                     PhotonNetwork.LocalPlayer.SetCustomProperties(props);
 
                     if (PhotonNetwork.IsMasterClient)
@@ -99,8 +99,8 @@ namespace NTNU.CarloMarton.VRLanguage
             {
                 object colorObject = GetColorOnPlayer(player);
                 if (colorObject != null)
-                { 
-                    takenColors.Add((Color) colorObject);
+                {
+                    takenColors.Add((Color)colorObject);
                 }
             }
 
@@ -141,7 +141,7 @@ namespace NTNU.CarloMarton.VRLanguage
             float[] colorAsList = { color.r, color.g, color.b, color.a };
             if (PhotonNetwork.LocalPlayer.ActorNumber == ownerId)
             {
-                Hashtable hash = new Hashtable() { {"Color", colorAsList } };
+                Hashtable hash = new Hashtable() { { "Color", colorAsList } };
                 PhotonNetwork.LocalPlayer.SetCustomProperties(hash);
             }
             UpdateAvailableColors();
@@ -169,7 +169,7 @@ namespace NTNU.CarloMarton.VRLanguage
 
         private Color GetColorOnPlayer(Player p)
         {
-            float[] colorAsFloat = (float[]) p.CustomProperties["Color"];
+            float[] colorAsFloat = (float[])p.CustomProperties["Color"];
             if (colorAsFloat == null)
             {
                 return Color.black;
